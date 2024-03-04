263cd9242dff016878d5039eb3379995d24dec3c
shimjaedoo <woen1637@naver.com>
2024-03-04 09:27:10 +0900

    feat : Post-Create DTO

---
+++ src/post/types/create/request.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class PostCreateDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(1)
+  description: string;
+}
